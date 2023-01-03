d613c912412d577d04908a9f1a96dba581e09f36
Aleksei Alferov <aleksei.alferov@encoway.de>
2023-01-03 22:21:26 +0100

    Add Unit Tests

--- src/test/java/guru/springframework/domain/CategoryTest.java
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -2,6 +2,8 @@ package guru.springframework.domain;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,9 +26,19 @@ public class CategoryTest {
 
     @Test
     public void testGetDescription() {
+        String description = "Some description";
+        classUnderTest.setDescription(description);
+
+        assertEquals(description, classUnderTest.getDescription());
     }
 
     @Test
     public void testGetRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+        classUnderTest.setRecipes(recipeData);
+
+        assertEquals(1, classUnderTest.getRecipes().size());
     }
 }
