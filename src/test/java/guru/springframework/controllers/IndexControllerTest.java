dce49a7ed4cc1419ee1b785c38ece70648a27952
Aleksei Alferov <aleksei.alferov@encoway.de>
2023-01-03 22:39:40 +0100

    Add Test for Index Controller

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+public class IndexControllerTest {
+
+    IndexController classUnderTest;
+
+    @Mock
+    Model model;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        classUnderTest = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+//        HashSet<Recipe> recipes = new HashSet<>();
+//        when(recipeService.getRecipes()).thenReturn(recipes);
+        String viewName = classUnderTest.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
