3e6048ab89d73ec25e0322bd4d51f4c52e9a51a9
Aleksei Alferov <aleksei.alferov@encoway.de>
2023-01-08 17:03:31 +0100

    Refactor

--- src/main/java/guru/springframework/commands/IngredientCommand.java
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -2,7 +2,6 @@ package guru.springframework.commands;
 
 import java.math.BigDecimal;
 
-import guru.springframework.domain.Recipe;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,5 +16,4 @@ public class IngredientCommand {
     private String description;
     private BigDecimal amount;
     private UnitOfMeasureCommand uom;
-    private Recipe recipe;
 }
