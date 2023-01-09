2e7370d822627f09f8149e18eff7458d34aacd5d
Aleksei Alferov <aleksei.alferov@encoway.de>
2023-01-09 23:03:57 +0100

    Add Update Ingredient

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -13,12 +13,9 @@ import javax.persistence.OneToOne;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
-@ToString
 @Entity
 @EqualsAndHashCode(exclude = "recipe")
 public class Ingredient {
